4606a04097d9b62cb4d4adedde199035c89ac7fb
PedroLBastos <pedrolsouza10@outlook.com>
2023-03-28 14:21:47 -0300

    add packages repository and service

---
+++ src/main/java/com/pedro/study/Repository/MyRepository.java
@@ -0,0 +1,7 @@
+package com.pedro.study.Repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MyRepository {
+}
---
+++ src/main/java/com/pedro/study/Service/MyService.java
@@ -0,0 +1,7 @@
+package com.pedro.study.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+}
