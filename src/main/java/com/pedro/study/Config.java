2302d4d94a4e706cf2e6d784e387b92106fa2dbb
PedroLBastos <pedrolsouza10@outlook.com>
2023-03-29 11:48:35 -0300

    working with different environments

--- src/main/java/com/pedro/study/Config.java
+++ src/main/java/com/pedro/study/Config.java
@@ -1,9 +1,20 @@
 package com.pedro.study;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("dev")
 public class Config {
+
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("Rodando dev");
+        };
+    }
+
 
 }
