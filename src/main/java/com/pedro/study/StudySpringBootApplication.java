6d9ce21696667ad042b67eee02d2195cf05651c0
PedroLBastos <pedrolsouza10@outlook.com>
2023-03-29 14:14:52 -0300

    working jdbc template

--- src/main/java/com/pedro/study/StudySpringBootApplication.java
+++ src/main/java/com/pedro/study/StudySpringBootApplication.java
@@ -1,13 +1,30 @@
 package com.pedro.study;
 
+import com.pedro.study.domain.entity.Cliente;
+import com.pedro.study.domain.repository.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class StudySpringBootApplication {
 
+	@Bean
+	public CommandLineRunner init (@Autowired Clientes clientes){
+		return args -> {
+			clientes.salvar(new Cliente("Pedro"));
+			clientes.salvar(new Cliente("Outro"));
+
+			List<Cliente> todos = clientes.obterTodos();
+			todos.forEach(System.out::println);
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudySpringBootApplication.class, args);
