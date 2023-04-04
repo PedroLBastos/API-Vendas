c6885c7393cffe8b0d3bd30f08adbccaeaa6af59
PedroLBastos <pedrolsouza10@outlook.com>
2023-04-04 13:16:04 -0300

    create request pedidos

--- src/main/java/com/pedro/study/rest/controller/PedidoController.java
+++ src/main/java/com/pedro/study/rest/controller/PedidoController.java
@@ -1,14 +1,26 @@
 package com.pedro.study.rest.controller;
 
+import com.pedro.study.domain.entity.ItemPedido;
 import com.pedro.study.domain.entity.Pedido;
+import com.pedro.study.rest.dto.InformacoesItemPedidoDTO;
+import com.pedro.study.rest.dto.InformacoesPedidoDTO;
 import com.pedro.study.rest.dto.PedidoDTO;
 import com.pedro.study.service.PedidoService;
 import org.springframework.http.HttpStatus;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/pedidos")
@@ -25,5 +37,39 @@ public class PedidoController {
     public Integer save(@RequestBody PedidoDTO dto){
        Pedido pedido = service.salvar(dto);
        return pedido.getId();
+    }
+
+    @GetMapping("/{id}")
+    public InformacoesPedidoDTO getById(@PathVariable Integer id){
+        return service
+                .obterPedidoCompleto(id)
+                .map(p -> converter(p))
+                .orElseThrow(()->
+                        new ResponseStatusException(HttpStatus.NOT_FOUND,"Pedido nao encontrado"));
+    }
+
+    private InformacoesPedidoDTO converter(Pedido pedido){
+        return InformacoesPedidoDTO
+                .builder()
+                .codigo(pedido.getId())
+                .datapedido(pedido.getDataPedido().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")))
+                .cpf(pedido.getCliente().getCpf())
+                .nomeCliente(pedido.getCliente().getNome())
+                .total(pedido.getTotal())
+                .itens(converter(pedido.getItens()))
+                .build();
+
+    }
+
+    private List<InformacoesItemPedidoDTO> converter(List<ItemPedido>itens){
+        if (CollectionUtils.isEmpty(itens)){
+            return Collections.emptyList();
+        }
+        return  itens.stream().map(item->InformacoesItemPedidoDTO
+                .builder().descricaoProduto(item.getProduto().getDescricao())
+                .precoUnitario(item.getProduto().getPreco())
+                .quantidade(item.getQuantidade())
+                .build()
+        ).collect(Collectors.toList());
     }
 }
--- src/main/java/com/pedro/study/service/PedidoService.java
+++ src/main/java/com/pedro/study/service/PedidoService.java
@@ -3,6 +3,10 @@ package com.pedro.study.service;
 import com.pedro.study.domain.entity.Pedido;
 import com.pedro.study.rest.dto.PedidoDTO;
 
+import java.util.Optional;
+
 public interface PedidoService {
     Pedido salvar (PedidoDTO dto);
+
+    Optional<Pedido> obterPedidoCompleto(Integer id);
 }
