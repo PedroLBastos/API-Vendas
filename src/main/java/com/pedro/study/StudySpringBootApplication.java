dccff09250fd4d847ff6aad8edd16c4ad2bd3d7d
PedroLBastos <pedrolsouza10@outlook.com>
2023-04-12 11:31:31 -0300

    configuration autentication in memory

--- src/main/java/com/pedro/study/StudySpringBootApplication.java
+++ src/main/java/com/pedro/study/StudySpringBootApplication.java
@@ -1,25 +1,12 @@
 package com.pedro.study;
 
-import com.pedro.study.domain.entity.Cliente;
-import com.pedro.study.domain.repository.Clientes;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudySpringBootApplication {
-
-    @Bean
-    public CommandLineRunner commandLineRunner(@Autowired Clientes clientes) {
-        return args -> {
-            Cliente c = new Cliente(null, "Pedro","111111111");
-            clientes.save(c);
-        };
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(StudySpringBootApplication.class, args);
